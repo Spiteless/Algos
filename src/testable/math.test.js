b78bc1aac22fd2753d38f312a885641f84515774
Dillon Arevalo <dillonbarevalo@gmail.com>
2020-01-03 19:36:03 -0800

    Add local babel-node to scripts using index.js

--- src/testable/math.test.js
+++ src/testable/math.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds -1 to 12 to get 11', () => {
   expect(sum(-1, 12)).toBe(11);
 });
+
+test('converts strings to numbers', () => {
+  expect(sum('1', '1')).toBe(2);
+});
